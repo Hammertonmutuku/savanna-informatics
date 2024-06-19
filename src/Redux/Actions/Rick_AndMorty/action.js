f5ff462d50aa6430ca146bf58747949f5538018d
Hammerton Muinde (Techno Brain Global FZ LLC) <v-hmuinde@microsoft.com>
2024-06-20 00:16:56 +0300

    added the action types for characters

---
+++ src/Redux/Actions/Rick_AndMorty/action.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const fetchCharacters = () => async dispatch => {
+  dispatch({ type: 'FETCH_CHARACTERS_REQUEST' });
+  try {
+    const response = await axios.get('https://rickandmortyapi.com/api/character');
+    dispatch({ type: 'FETCH_CHARACTERS_SUCCESS', payload: response.data });
+  } catch (error) {
+    dispatch({ type: 'FETCH_CHARACTERS_FAILURE', payload: error.message });
+  }
+};
+
+export const fetchCharacter = (id) => async dispatch => {
+  dispatch({ type: 'FETCH_CHARACTERS_REQUEST' });
+  try {
+    const response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
+    dispatch({ type: 'FETCH_CHARACTERS_SUCCESS', payload: response.data });
+  } catch (error) {
+    dispatch({ type: 'FETCH_CHARACTERS_FAILURE', payload: error.message });
+  }
+};
+
+export const fetchMultipleCharacters = (id, userData) => async dispatch => {
+//   dispatch({ type: 'EDIT_USER_REQUEST' });
+//   try {
+//     const response = await axios.patch(`https://lionfish-app-qkntx.ondigitalocean.app/api/user/${id}`, userData);
+//     dispatch({ type: 'EDIT_USER_SUCCESS', payload: response.data });
+//   } catch (error) {
+//     dispatch({ type: 'EDIT_USER_FAILURE', payload: error.message });
+//   }
+};
