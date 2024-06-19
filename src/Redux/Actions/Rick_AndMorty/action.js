f1a359bd6e558c34275def1bb7d5655680503719
Hammerton Muinde (Techno Brain Global FZ LLC) <v-hmuinde@microsoft.com>
2024-06-20 00:22:58 +0300

    added the action types for epidoes

--- src/Redux/Actions/Rick_AndMorty/action.js
+++ src/Redux/Actions/Rick_AndMorty/action.js
@@ -29,3 +29,24 @@ export const fetchMultipleCharacters = (id, userData) => async dispatch => {
 //     dispatch({ type: 'EDIT_USER_FAILURE', payload: error.message });
 //   }
 };
+
+export const fetchEpisodes = () => async dispatch => {
+    dispatch({ type: 'FETCH_EPISODES_REQUEST' });
+    try {
+      const response = await axios.get('https://rickandmortyapi.com/api/episode');
+      dispatch({ type: 'FETCH_EPISODES_SUCCESS', payload: response.data });
+    } catch (error) {
+      dispatch({ type: 'FETCH_EPISODES_FAILURE', payload: error.message });
+    }
+  };
+
+  export const fetchEpisode = (id) => async dispatch => {
+    dispatch({ type: 'FETCH_EPISODES_REQUEST' });
+    try {
+      const response = await axios.get(`https://rickandmortyapi.com/api/episode/${id}`);
+      dispatch({ type: 'FETCHE_PISODES_SUCCESS', payload: response.data });
+    } catch (error) {
+      dispatch({ type: 'FETCH_EPISODES_FAILURE', payload: error.message });
+    }
+  };
+
